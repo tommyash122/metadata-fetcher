676baf9b9022a0bfec3e0ee611aa242690ee901e
Tommy Ashkenazi <84855441+tommyash122@users.noreply.github.com>
2024-08-23 01:09:54 +0300

    bf color

--- client/src/components/form/MetadataDisplay.js
+++ client/src/components/form/MetadataDisplay.js
@@ -4,10 +4,16 @@ function MetadataDisplay({ metadata }) {
   return (
     <div>
       {metadata.map((data, index) => (
-        <div key={index}>
-          <h3>{DOMPurify.sanitize(data.title)}</h3>
-          <p>{DOMPurify.sanitize(data.description)}</p>
-          {data.image && <img src={DOMPurify.sanitize(data.image)} alt={`Thumbnail for ${data.title}`} />}
+        <div key={index} className="mb-6">
+          <h3 className="text-xl font-bold mb-2">{DOMPurify.sanitize(data.title)}</h3>
+          <p className="mb-4">{DOMPurify.sanitize(data.description)}</p>
+          {data.image && (
+            <img
+              src={DOMPurify.sanitize(data.image)}
+              alt={`Thumbnail for ${data.title}`}
+              className="max-w-full h-auto max-h-80 object-cover border-4 border-gray-300 rounded-lg shadow-lg"
+            />
+          )}
         </div>
       ))}
     </div>
