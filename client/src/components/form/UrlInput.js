8ef0792d5affb62e231c6b5ca5097f539fb894c8
Tommy Ashkenazi <84855441+tommyash122@users.noreply.github.com>
2024-08-22 21:57:16 +0300

    purple lines

--- client/src/components/form/UrlInput.js
+++ client/src/components/form/UrlInput.js
@@ -8,14 +8,14 @@ function UrlInput({ index, value, onChange, onRemoveUrl, showRemoveButton }) {
         value={value}
         onChange={(e) => onChange(index, e.target.value)}
         placeholder={`https://example${index + 1}.com`}
-        className="border border-purple-300 rounded p-2 flex-grow"
+        className="border border-purple-300 rounded p-2 flex-grow focus:border-purple-700 focus:ring-1 focus:ring-purple-600 focus:outline-none"
         required
       />
       {showRemoveButton && (
         <button
           type="button"
           onClick={() => onRemoveUrl(index)}
-          className="bg-red-500 text-white px-4 py-2 rounded">
+          className="bg-gray-500 text-white px-6 py-2 rounded">
           Remove
         </button>
       )}
