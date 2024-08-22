f56b31f1c82895a0503fdfcb0cc17aee7a6fee18
Tommy Ashkenazi <84855441+tommyash122@users.noreply.github.com>
2024-08-22 22:08:58 +0300

    lucide copy and padding

--- client/src/components/form/UrlInput.js
+++ client/src/components/form/UrlInput.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import { Copy } from 'lucide-react';
 
 function UrlInput({ index, value, onChange, onRemoveUrl, showRemoveButton }) {
+  const handleCopy = () => {
+    navigator.clipboard.writeText(value);
+    alert('Copied to clipboard');
+  };
+
   return (
     <div className="flex items-center space-x-2 mb-4">
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => onChange(index, e.target.value)}
-        placeholder={`https://example${index + 1}.com`}
-        className="border border-purple-300 rounded p-2 flex-grow focus:border-purple-700 focus:ring-1 focus:ring-purple-600 focus:outline-none"
-        required
-      />
+      <div className="relative flex-grow">
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => onChange(index, e.target.value)}
+          placeholder={`https://example${index + 1}.com`}
+          className="border border-purple-300 rounded p-2 w-full focus:border-purple-700 focus:ring-1 focus:ring-purple-600 focus:outline-none pr-10"
+          required
+        />
+        {value && (
+          <button 
+            type="button" 
+            onClick={handleCopy} 
+            className="absolute right-2 top-1/2 transform -translate-y-1/2 text-purple-700">
+            <Copy size={20} />
+          </button>
+        )}
+      </div>
       {showRemoveButton && (
         <button
           type="button"
