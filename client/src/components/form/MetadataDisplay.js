60c1e5c1f33f59ca758bdcd42752771616bacce2
Tommy Ashkenazi <84855441+tommyash122@users.noreply.github.com>
2024-08-23 01:12:47 +0300

    cards layout

--- client/src/components/form/MetadataDisplay.js
+++ client/src/components/form/MetadataDisplay.js
@@ -2,16 +2,18 @@ import DOMPurify from 'dompurify';
 
 function MetadataDisplay({ metadata }) {
   return (
-    <div>
+    <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       {metadata.map((data, index) => (
-        <div key={index} className="mb-6">
-          <h3 className="text-xl font-bold mb-2">{DOMPurify.sanitize(data.title)}</h3>
-          <p className="mb-4">{DOMPurify.sanitize(data.description)}</p>
+        <div key={index} className="bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300">
+          <div className="p-4">
+            <h3 className="text-2xl font-semibold text-gray-800 mb-3">{DOMPurify.sanitize(data.title)}</h3>
+            <p className="text-gray-600 mb-4">{DOMPurify.sanitize(data.description)}</p>
+          </div>
           {data.image && (
             <img
               src={DOMPurify.sanitize(data.image)}
               alt={`Thumbnail for ${data.title}`}
-              className="max-w-full h-auto max-h-80 object-cover border-4 border-gray-300 rounded-lg shadow-lg"
+              className="w-full h-auto max-h-60 object-cover border-t border-gray-200"
             />
           )}
         </div>
