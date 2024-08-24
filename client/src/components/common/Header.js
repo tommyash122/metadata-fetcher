b9bcf243c709970bb20069abb088f61735f1fc20
Tommy Ashkenazi <84855441+tommyash122@users.noreply.github.com>
2024-08-24 17:22:51 +0300

    animation

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -9,22 +9,22 @@ function Header() {
                     Metadata Fetcher
                 </NavLink>
                 <nav className="flex space-x-4 ml-8">
-                    <NavLink
-                        to="/"
-                        className={({ isActive }) =>
-                            isActive
-                                ? "text-xl text-purple-700 underline"
-                                : "text-xl text-purple-500 hover:text-purple-700 hover:underline"
+                    <NavLink 
+                        to="/" 
+                        className={({ isActive }) => 
+                            isActive 
+                            ? "relative text-xl text-purple-700 after:absolute after:left-0 after:bottom-0 after:h-0.5 after:bg-purple-700 after:w-full after:transform after:scale-x-100 after:origin-left after:transition-transform after:duration-300"
+                            : "relative text-xl text-purple-500 hover:bg-purple-100 hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:h-0.5 hover:after:bg-purple-700 hover:after:w-full hover:after:transform hover:after:scale-x-0 hover:after:origin-left hover:after:transition-transform hover:after:duration-300"
                         }
                     >
                         Home
                     </NavLink>
-                    <NavLink
-                        to="/about"
-                        className={({ isActive }) =>
-                            isActive
-                                ? "text-xl text-purple-700 underline"
-                                : "text-xl text-purple-500 hover:text-purple-700 hover:underline"
+                    <NavLink 
+                        to="/about" 
+                        className={({ isActive }) => 
+                            isActive 
+                            ? "relative text-xl text-purple-700 after:absolute after:left-0 after:bottom-0 after:h-0.5 after:bg-purple-700 after:w-full after:transform after:scale-x-100 after:origin-left after:transition-transform after:duration-300"
+                            : "relative text-xl text-purple-500 hover:text-purple-700 hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:h-0.5 hover:after:bg-purple-700 hover:after:w-full hover:after:transform hover:after:scale-x-0 hover:after:origin-left hover:after:transition-transform hover:after:duration-300"
                         }
                     >
                         About
