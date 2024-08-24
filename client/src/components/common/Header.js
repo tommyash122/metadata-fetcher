c2979bc4732cae4ee66ba7f43f025aa129386955
Tommy Ashkenazi <84855441+tommyash122@users.noreply.github.com>
2024-08-24 16:56:41 +0300

    items size

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <header className="bg-gray-200 sticky top-0 z-20 w-full border-b border-gray-500 p-4">
-            <div className="container mx-auto flex justify-between items-center">
+            <div className="container mx-auto flex items-center">
                 <h1 className="text-4xl font-extrabold text-purple-400 text-center">
                     Metadata Fetcher
                 </h1>
-                <nav className="flex space-x-4">
-                    <Link to="/" className="text-purple-500 hover:text-purple-700 hover:underline">Home</Link>
-                    <Link to="/about" className="text-purple-500 hover:text-purple-700 hover:underline">About</Link>
+                <nav className="flex space-x-4 ml-8">
+                    <Link to="/" className="text-xl text-purple-500 hover:text-purple-700 hover:underline">Home</Link>
+                    <Link to="/about" className="text-xl text-purple-500 hover:text-purple-700 hover:underline">About</Link>
                 </nav>
             </div>
         </header>
