e251711d0e93f2b4ceaebc8bfb1482744a90cfe7
Tommy Ashkenazi <84855441+tommyash122@users.noreply.github.com>
2024-10-30 19:45:28 +0200

    new design

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -3,18 +3,18 @@ import { NavLink } from 'react-router-dom';
 
 function Header() {
     return (
-        <header className="bg-gray-200 sticky top-0 z-20 w-full border-b border-gray-500 p-4">
-            <div className="container mx-auto flex items-center">
-                <NavLink to="/" className="text-4xl font-extrabold text-purple-400 text-center">
+        <header className="bg-gradient-to-r from-purple-400 to-purple-600 sticky top-0 z-20 w-full shadow-md p-4">
+            <div className="container mx-auto flex items-center justify-between">
+                <NavLink to="/" className="text-5xl font-extrabold text-white drop-shadow-md">
                     Metadata Fetcher
                 </NavLink>
-                <nav className="flex space-x-4 ml-8">
+                <nav className="hidden sm:flex space-x-6 ml-8">
                     <NavLink 
                         to="/" 
                         className={({ isActive }) => 
                             isActive 
-                            ? "relative text-xl text-purple-700 after:absolute after:left-0 after:bottom-0 after:h-0.5 after:bg-purple-700 after:w-full after:transform after:scale-x-100 after:origin-left after:transition-transform after:duration-300"
-                            : "relative text-xl text-purple-500 hover:bg-purple-100 hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:h-0.5 hover:after:bg-purple-700 hover:after:w-full hover:after:transform hover:after:scale-x-0 hover:after:origin-left hover:after:transition-transform hover:after:duration-300"
+                            ? "relative text-lg text-white after:absolute after:left-0 after:bottom-0 after:h-0.5 after:bg-white after:w-full after:scale-x-100 after:transition-transform after:duration-300"
+                            : "relative text-lg text-white opacity-80 hover:opacity-100 hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:h-0.5 hover:after:bg-white hover:after:w-full hover:after:transform hover:after:scale-x-0 hover:after:transition-transform hover:duration-300"
                         }
                     >
                         Home
@@ -23,13 +23,19 @@ function Header() {
                         to="/about" 
                         className={({ isActive }) => 
                             isActive 
-                            ? "relative text-xl text-purple-700 after:absolute after:left-0 after:bottom-0 after:h-0.5 after:bg-purple-700 after:w-full after:transform after:scale-x-100 after:origin-left after:transition-transform after:duration-300"
-                            : "relative text-xl text-purple-500 hover:bg-purple-100 hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:h-0.5 hover:after:bg-purple-700 hover:after:w-full hover:after:transform hover:after:scale-x-0 hover:after:origin-left hover:after:transition-transform hover:after:duration-300"
+                            ? "relative text-lg text-white after:absolute after:left-0 after:bottom-0 after:h-0.5 after:bg-white after:w-full after:scale-x-100 after:transition-transform after:duration-300"
+                            : "relative text-lg text-white opacity-80 hover:opacity-100 hover:after:absolute hover:after:left-0 hover:after:bottom-0 hover:after:h-0.5 hover:after:bg-white hover:after:w-full hover:after:transform hover:after:scale-x-0 hover:after:transition-transform hover:duration-300"
                         }
                     >
                         About
                     </NavLink>
                 </nav>
+                {/* Hamburger Menu for Mobile */}
+                <div className="sm:hidden">
+                    <button className="text-white">
+                        {/* Add a hamburger icon here, e.g., from Lucide or Heroicons */}
+                    </button>
+                </div>
             </div>
         </header>
     );
