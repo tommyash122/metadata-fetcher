eebe33980b2f28997b8fc0027bde485836a632c8
Tommy Ashkenazi <84855441+tommyash122@users.noreply.github.com>
2024-11-01 12:29:18 +0200

    allowedOrigins

--- server/server.js
+++ server/server.js
@@ -30,13 +30,7 @@ const allowedOrigins = [
 
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: allowedOrigins,
   credentials: true,
 }));
 
